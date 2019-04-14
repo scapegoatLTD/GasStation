2f391405f761b311737d9de55fb74fc431ce7cdb
GRVN <grvn@bti>
2019-04-14 20:16:34 +0200

    First Commit

---
+++ GasStation/src/exe/GasStation.java
@@ -0,0 +1,21 @@
+/**
+ * @file     GasStation.java
+ * @date     14.04.2019
+ */
+package exe;
+
+/**
+ * @author BTIndustries
+ * @version 0.1 - 2019
+ */
+public class GasStation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
